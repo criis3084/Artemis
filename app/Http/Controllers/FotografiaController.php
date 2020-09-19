d9c3500e84191cfa278e13a415aa1f687828cad5
Jonhy07 <jonhy.simaj@gmail.com>
2020-09-18 18:14:29 -0600

    primeras apis

--- app/Http/Controllers/FotografiaController.php
+++ app/Http/Controllers/FotografiaController.php
@@ -4,29 +4,60 @@ namespace App\Http\Controllers;
 
 use App\Fotografia;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+use Exception;
 
 class FotografiaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+	public function index(Request $request)
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
+		// Filtro por un criterio y estado
+        $buscar = $request->buscar;
+        $criterio = $request->criterio;
+		
+		if ($buscar==''){
+			$fotografia = Fotografia::orderBy('id', 'desc')->where('estado',1)->paginate(20);
+		}
+		else{
+			$fotografia = Fotografia::where($criterio, 'like', '%'. $buscar . '%')->orderBy('id', 'desc')->paginate(20);
+		}
+        return [
+            'pagination' => [
+                'total'        => $fotografia->total(),
+                'current_page' => $fotografia->currentPage(),
+                'per_page'     => $fotografia->perPage(),
+                'last_page'    => $fotografia->lastPage(),
+                'from'         => $fotografia->firstItem(),
+                'to'           => $fotografia->lastItem(),
+            ],
+            'fotografias' => $fotografia
+		];
+		
+		// Filtro solo por un criterio 
+		/*
+		$buscar = $request->buscar;
+        $criterio = $request->criterio;
+		
+			if ($buscar==''){
+				$escuela = Escuela::orderBy('id', 'desc')->where('estado',1)->paginate(20);
+			}
+			else{
+				$escuela = Escuela::where($criterio, 'like', '%'. $buscar . '%')->orderBy('id', 'desc')->paginate(20);
+			}
+        return [
+            'pagination' => [
+                'total'        => $escuela->total(),
+                'current_page' => $escuela->currentPage(),
+                'per_page'     => $escuela->perPage(),
+                'last_page'    => $escuela->lastPage(),
+                'from'         => $escuela->firstItem(),
+                'to'           => $escuela->lastItem(),
+            ],
+            'aldeas' => $escuela
+		];
+		*/
+	}
+	
     /**
      * Store a newly created resource in storage.
      *
@@ -35,7 +66,21 @@ class FotografiaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+		/*
+		if(!$request->ajax())return redirect('/');
+        try {
+			*/
+			$fotografia = new Fotografia();
+			$fotografia->ruta = $request->ruta;
+			$fotografia->descripcion = $request->descripcion;
+			$fotografia->titulo = $request->titulo;
+			$fotografia->save();
+			return Response::json(['message' => 'Fotografia Subida'], 200);
+		/*
+		} catch (Exception $e) {
+            return Response::json(['message' => $e->getMessage()], 400);
+		}
+		*/
     }
 
     /**
@@ -46,20 +91,13 @@ class FotografiaController extends Controller
      */
     public function show(Fotografia $fotografia)
     {
-        //
+		return [
+			'id'=> $fotografia->id,
+			'ruta'=> $fotografia->ruta,
+			'titulo'=> $fotografia->titulo,
+			'descripcion'=> $fotografia->descripcion
+		];
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Fotografia  $fotografia
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Fotografia $fotografia)
-    {
-        //
-    }
-
     /**
      * Update the specified resource in storage.
      *
@@ -69,6 +107,12 @@ class FotografiaController extends Controller
      */
     public function update(Request $request, Fotografia $fotografia)
     {
+		$fotografia = Fotografia::findOrFail($request->id);
+		$fotografia->ruta = $request->ruta;
+		$fotografia->titulo = $request->titulo;
+		$fotografia->descripcion = $request->descripcion;
+		$fotografia->save();
+		return Response::json(['message' => 'Fotografia Actualizada'], 200);
         //
     }
 
@@ -80,6 +124,10 @@ class FotografiaController extends Controller
      */
     public function destroy(Fotografia $fotografia)
     {
-        //
+		//if(!$request->ajax())return redirect('/');
+        $fotografia = Fotografia::findOrFail($fotografia->id);
+        $fotografia->estado = '0';
+		$fotografia->save();
+		return Response::json(['message' => 'Fotografia Desactivada'], 200);
     }
 }
--- app/Http/Controllers/PersonaSinAccesoController.php
+++ app/Http/Controllers/PersonaSinAccesoController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\PersonaSinAcceso;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+use Exception;
 
 class PersonaSinAccesoController extends Controller
 {
@@ -12,19 +14,40 @@ class PersonaSinAccesoController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+				// Filtro por un criterio y estado
+				$buscar = $request->buscar;
+				$criterio = $request->criterio;
+				$completo = $request->completo;
+				
+				if ($completo == 'false')
+				{
+					if ($buscar==''){
+						$persona_sin_acceso = PersonaSinAcceso::orderBy('id', 'desc')->where('estado',1)->paginate(20);
+					}
+					else{
+						$persona_sin_acceso = PersonaSinAcceso::where($criterio, 'like', '%'. $buscar . '%')->orderBy('id', 'desc')->paginate(20);
+					}
+				} else if ($completo == 'true'){
+					if ($buscar==''){
+						$persona_sin_acceso = PersonaSinAcceso::orderBy('id', 'desc')->paginate(20);
+					}
+					else{
+						$persona_sin_acceso = PersonaSinAcceso::where($criterio, 'like', '%'. $buscar . '%')->orderBy('id', 'desc')->paginate(20);
+					}
+				}
+				return [
+					'pagination' => [
+						'total'        => $persona_sin_acceso->total(),
+						'current_page' => $persona_sin_acceso->currentPage(),
+						'per_page'     => $persona_sin_acceso->perPage(),
+						'last_page'    => $persona_sin_acceso->lastPage(),
+						'from'         => $persona_sin_acceso->firstItem(),
+						'to'           => $persona_sin_acceso->lastItem(),
+					],
+					'personas' => $persona_sin_acceso
+				];        
     }
 
     /**
@@ -35,7 +58,26 @@ class PersonaSinAccesoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+				/*
+		if(!$request->ajax())return redirect('/');
+        try {
+			*/
+			$persona = new PersonaSinAcceso();
+			$persona->nombres = $request->nombres;
+			$persona->apellidos = $request->apellidos;
+			$persona->CUI = $request->CUI;
+			$persona->genero = $request->genero;
+			$persona->fecha_nacimiento = $request->fecha_nacimiento;
+			$persona->direccion = $request->direccion;
+			$persona->numero_telefono = $request->numero_telefono;
+			$persona->sector_id = $request->sector_id;
+			$persona->save();
+			return Response::json(['message' => 'Persona Creada'], 200);
+		/*
+		} catch (Exception $e) {
+            return Response::json(['message' => $e->getMessage()], 400);
+		}
+		*/
     }
 
     /**
@@ -51,27 +93,16 @@ class PersonaSinAccesoController extends Controller
         return[
 			"nombres" => $personaSinAcceso->nombres,
 			"apellidos"=>$personaSinAcceso->apellidos,
+			"CUI"=>$personaSinAcceso->CUI,
 			"genero"=>$personaSinAcceso->genero,
 			"fecha_nacimiento"=>$personaSinAcceso->fecha_nacimiento,
 			"direccion"=>$personaSinAcceso->direccion,
 			"numero_telefono"=>$personaSinAcceso->numero_telefono,
-			'Sector' => $personaSinAcceso->sector,
-			'nombre_sector' => $personaSinAcceso->sector->nombre,
-			'nino' => $nino,
+			'sector_id' => $personaSinAcceso->sector_id,
+			'sector_nombre' => $personaSinAcceso->sector->nombre
 		];
     }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\PersonaSinAcceso  $personaSinAcceso
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(PersonaSinAcceso $personaSinAcceso)
-    {
-        //
-    }
-
+                           
     /**
      * Update the specified resource in storage.
      *
@@ -81,7 +112,17 @@ class PersonaSinAccesoController extends Controller
      */
     public function update(Request $request, PersonaSinAcceso $personaSinAcceso)
     {
-        //
+		$persona = PersonaSinAcceso::findOrFail($request->id);
+		$persona->nombres = $request->nombres;
+		$persona->apellidos = $request->apellidos;
+		$persona->CUI = $request->CUI;
+		$persona->genero = $request->genero;
+		$persona->fecha_nacimiento = $request->fecha_nacimiento;
+		$persona->direccion = $request->direccion;
+		$persona->numero_telefono = $request->numero_telefono;
+		$persona->sector_id = $request->sector_id;
+		$persona->save();
+		return Response::json(['message' => 'Sector Actualizada'], 200);
     }
 
     /**
@@ -92,6 +133,9 @@ class PersonaSinAccesoController extends Controller
      */
     public function destroy(PersonaSinAcceso $personaSinAcceso)
     {
-        //
+		$personaSinAcceso = PersonaSinAcceso::findOrFail($personaSinAcceso->id);
+        $personaSinAcceso->estado = '0';
+		$personaSinAcceso->save();
+		return Response::json(['message' => 'Persona Desactivado'], 200);
     }
 }
