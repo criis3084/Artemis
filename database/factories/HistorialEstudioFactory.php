abbcef4be5a7aee40290a86ebf14573c901c7550
PsychedVic <v-mas10@hotmail.com>
2020-10-07 00:45:07 -0600

    Se movió la relación de Estudio a Niño... Otros arreglos en tablas de vivienda

--- database/factories/HistorialEstudioFactory.php
+++ database/factories/HistorialEstudioFactory.php
@@ -10,7 +10,7 @@ use Faker\Generator as Faker;
 $factory->define(HistorialEstudio::class, function (Faker $faker) {
     return [
         'estudio_socioeconomico_id' => factory(App\EstudioSocioeconomico::class),
-        'relacion_id' => factory(App\Nino::class),
+        'nino_id' => factory(App\Nino::class),
         'estado' => $faker->numberBetween($min = 0, $max = 1),
         'created_at' => $faker->dateTime($max = 'now', $timezone = null),
         'updated_at' => $faker->dateTime($max = 'now', $timezone = null),
