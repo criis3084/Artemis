091eea6e68155562ffd722c5870326f0eedadb0b
PsychedVic <v-mas10@hotmail.com>
2020-10-18 22:06:49 -0600

    Arreglos controladores Detalle I/O

--- resources/js/src/views/apadrinamientos/familia/estudio_familia.vue
+++ resources/js/src/views/apadrinamientos/familia/estudio_familia.vue
@@ -8,10 +8,17 @@
 			</div>
 
 			<div class="vx-col md:w-1/2 w-full mt-6">
+				<small class="date-label">Total de ingresos mensuales</small>
+				<vx-input-group class="mb-base">
+   				 <template slot="prepend">	
+				<div class="prepend-text bg-primary" >
+					<span>Q</span>	
+				</div>
 				<div class="vx-col w-full">
-					<small class="date-label">Total de ingresos mensuales</small>
 					<vs-input class="w-full" icon-pack="feather" icon="icon-dollar-sign" icon-no-border name='total_ingresos' v-model="total_ingresos"/>
 				</div>
+				</template>
+  				</vx-input-group>
 			</div>
 
 			<div class="vx-col md:w-1/2 w-full mt-6">
@@ -33,9 +40,17 @@
 			</vs-list>
 
 			<div class="vx-col md:w-1/2 w-full mt-6">
-				<div class="vx-col w-full">
-					<vs-input class="w-full" icon-pack="feather" icon="icon-dollar-sign" icon-no-border label-placeholder="Costos de la vivienda" name='descripcion_costo' v-model="descripcion_costo"/>
+				<small class="date-label">Costos de la vivienda</small>
+				<vx-input-group class="mb-base">
+   				 <template slot="prepend">	
+				<div class="prepend-text bg-primary" >
+					<span>Q</span>	
 				</div>
+				<div class="vx-col w-full">
+					<vs-input class="w-full" name='descripcion_costo' v-model="descripcion_costo"/>
+				</div>
+				</template>
+  				</vx-input-group>
 			</div>
 
 			<vs-list class="mt-6">
