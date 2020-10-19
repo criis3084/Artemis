040b68546d9a6ea41fc1c0845df14bcf780ad9f2
PsychedVic <v-mas10@hotmail.com>
2020-10-19 12:54:16 -0600

    Factory de Beneficio

--- database/factories/BeneficioFactory.php
+++ database/factories/BeneficioFactory.php
@@ -8,7 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(Beneficio::class, function (Faker $faker) {
     return [
 		'descripcion' => $faker->text($maxNbChars = 100), 
-		'fecha_entrega' => $faker->date($format = 'Y-m-d', $max = 'now'),
+		'fecha_entrega' => $faker->dateTimeBetween($startDate = '-9 months', $endDate = 'now', $timezone = null),
         'paciente_id' => Paciente::pluck('id')[$faker->numberBetween(1,Paciente::count()-1)],
         'estado' => $faker->numberBetween($min = 0, $max = 1)
     ];
