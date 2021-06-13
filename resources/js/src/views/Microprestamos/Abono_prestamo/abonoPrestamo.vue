93724e6203f2797fa380bbd573be475894cb4ee8
MacoRios <riosmaco518@gmail.com>
2021-06-13 17:19:08 -0600

    juan hueco

--- resources/js/src/views/Microprestamos/Abono_prestamo/abonoPrestamo.vue
+++ resources/js/src/views/Microprestamos/Abono_prestamo/abonoPrestamo.vue
@@ -24,7 +24,7 @@
             <span>Cantidad de abono(Q)</span>
           </div>
           <div class="vx-col sm:w-2/3 w-full">
-            <vs-input type="number" class="w-full" v-model="cantidad_abono"  name="cantidad"/>
+            <vs-input type="number" class="w-full" v-model="cantidad_abono"  name="cantidad" v-validate="'required|max:5|numeric'"/>
             <span class="text-danger text-sm" v-show="errors.has('cantidad')">{{ errors.first('cantidad') }}</span>
             <span v-if ="dias > 0 && mes === 0" >{{ 'Dias atrasados:'+ ' ' + dias+ ' '}}</span>
             <span v-if="mes > 0" >{{'Meses atrasados:'+' '+ mes}} </span>
@@ -132,11 +132,11 @@ import Ls from '../../../services/ls';
 import { Validator } from 'vee-validate'
 const dict = {
   custom: {
-   /* cantidad: {
+    cantidad: {
       	  required: 'El campo cantidad de abono es requerido',
-      	 //numeric: 'El campo solo debe de contener números',
+      	  numeric: 'El campo solo debe de contener números',
       max: 'Este campo solo acepta hasta 5 dígitos'
-    },*/
+    },
     descripcion: {
       required:'El campo descripción es requerido',
       max:'No se aceptan más de 150 caracteres'
